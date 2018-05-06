eed08e318bbe4345f1629a8f31258d1e8193b2fd
Sambhav Jain <sambhavjain2612@gmail.com>
2018-05-06 18:26:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = robot => {
-  robot.on('pull_request.opened', receive);
-  async function receive(context) {
+  robot.on('pull_request.o>pened', async context => {
       // Get all issues for repo with user as creator
       const response = await context.github.issues.getForRepo(context.repo({
           state: 'all',
@@ -21,4 +20,24 @@ module.exports = robot => {
           }
       }
   }
+  robot.on('issues.opened', async context => {
+    const response = await context.github.issues.getForRepo(context.repo({
+      state: 'all',
+      creator: context.payload.issue.user.login
+    }))
+
+    const countIssue = response.data.filter(data => !data.pull_request)
+    if (countIssue.length === 1) {
+      try {
+        const config = await context.config('config.yml')
+        if (config && config.newIssueWelcomeComment) {
+          context.github.issues.createComment(context.issue({body: config.newIssueWelcomeComment}))
+        }
+      } catch (err) {
+        if (err.code !== 404) {
+          throw err
+        }
+      }
+    }
+})
 };
