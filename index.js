324e78d3ddd50cc2e40443cb6eceeb8fe24bb379
sambhav2612 <sambhavjain2612@gmail.com>
2018-05-06 17:48:59 +0530

    update app

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
+module.exports = robot => {
+  robot.on('pull_request.opened', receive);
+  async function receive(context) {
+      // Get all issues for repo with user as creator
+      const response = await context.github.issues.getForRepo(context.repo({
+          state: 'all',
+          creator: context.payload.pull_request.user.login
+      }));
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+      const countPR = response.data.filter(data => data.pull_request);
+      if (countPR.length === 1) {
+          try {
+              const config = await context.config('config.yml');
+              if (config.newPRWelcomeComment) {
+                  context.github.issues.createComment(context.issue({body: config.newPRWelcomeComment}));
+              }
+          } catch (err) {
+              if (err.code !== 404) {
+                  throw err;
+              }
+          }
+      }
+  }
+};
